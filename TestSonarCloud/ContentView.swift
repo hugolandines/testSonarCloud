5104beb48cfb16fbf5643bcd26170dabb7acc85f
Hugo Landines <hugolandines@gmail.com>
2020-05-11 16:39:38 -0500

    Initial Commit

---
+++ TestSonarCloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestSonarCloud
+//
+//  Created by Hugo Landines on 5/11/20.
+//  Copyright © 2020 hugo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
